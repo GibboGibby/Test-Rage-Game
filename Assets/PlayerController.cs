cdd3cd8b2dacb7737e8fd773157fdc8786b86191
GibboGibby <james@netcom.co.uk>
2023-12-15 10:03:42 +0000

    Fixed player controller not moving

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -149,6 +149,7 @@ public class PlayerController : MonoBehaviour
             {
                 lr.positionCount = 0;
                 canFire = false;
+                readying = false;
                 Fire();
             }
         }
