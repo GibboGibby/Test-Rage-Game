577da4d427f3fe3d0d95dfe52f9cd310c0e0269b
GibboGibby <james@netcom.co.uk>
2023-12-31 15:51:03 +0000

    Added a leaderboard plugin as well as dans font(it was bad tho)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private LeaderboardManager leaderboardManager;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public LeaderboardManager GetLeaderboardManager()
+    {
+        return leaderboardManager;
+    }
+}
---
+++ Assets/Scripts/LeaderboardManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dan.Main;
+
+public class LeaderboardManager : MonoBehaviour
+{
+    private string userNameKey = "Username";
+
+    private string publicKey = "73a86103165174701f5470c18f8d14d4f9e4aa1887fbfe12b39c1cd9992ab283";
+    private string secretKey = "7ac321de3aa7a00142eb9c6bf5cca075a1d8c3ed68796271a6a5ef9e7cc012a5042382bf3e4ab09dba40da3350fe739c75d6b9a50cbe5dba7faec78559ac671d83780452f4cf4e21202dd9096885b59b3a4c806cdab5fe828c47b2cd3d9a0ad741b92d0b914692b517ac9730604fcc0907f4e981acd8aca3304468852a14b2ca";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey(userNameKey))
+        {
+            Debug.Log("Has key");
+        }
+        else
+        {
+            string player = string.Empty;
+            player = "Player" + Random.Range(1000, 10000000).ToString();
+            PlayerPrefs.SetString(userNameKey, player);
+        }
+    }
+
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicKey, ((msg) =>
+        {
+            Debug.Log(msg);
+        }));
+    }
+
+    public void AddNewEntry(string username, int height, int time)
+    {
+        LeaderboardCreator.UploadNewEntry(publicKey, username, height, ((msg) =>
+        {
+            GetLeaderboard();
+        }));
+    }
+
+    public void ChangeName(string name)
+    {
+        //string oldName = PlayerPrefs.GetString(userNameKey);
+        PlayerPrefs.SetString(userNameKey, name);
+        LeaderboardCreator.UpdateEntryUsername(publicKey, name);
+    }
+
+    int height = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            AddNewEntry(PlayerPrefs.GetString(userNameKey), height, 0);
+            height++;
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            ChangeName("Jame");
+        }
+    }
+}
